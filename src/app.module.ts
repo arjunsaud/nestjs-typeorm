1aae2f655ae8adf93dc9bc2a68e26aa394e17752
arjunsaud <arundk055@gmail.com>
2024-01-02 21:08:54 +0545

    entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ItemsModule } from './items/items.module';
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ItemsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ItemsModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
